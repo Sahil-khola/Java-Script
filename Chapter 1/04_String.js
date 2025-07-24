e8788b3c1ff65f62b465394bf5f8af76f61bc09b
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-24 23:20:09 +0530

    JAVASCRIPT

---
+++ Chapter 1/04_String.js
@@ -0,0 +1,31 @@
+let firstName = "    Sahil";
+let lastName = "Khola";
+// console.log(firstName,lastName);
+
+//concatination string----------------------->
+
+// # using + oprator
+// let fullName = firstName + " "+lastName;
+// console.log(fullName);
+
+//  #using template literal
+// console.log(`My Name is ${firstName} ${lastName}`);
+
+
+// get string character--------------------------->
+// console.log(firstName[0]);
+// console.log(firstName[1]);
+// console.log(firstName[2]);
+// console.log(firstName[3]);
+// console.log(firstName[4]);
+// console.log(firstName[5]);
+
+// String Method ------------------>
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+// console.log(lastName.indexOf("o"));
+
+// # TRIM -----------> it remove unwanted space
+console.log(firstName);
+console.log(firstName.trim());
+ 
