fa9b9e572cf43d545dca55b8ff9d4e519f0a2947
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-29 23:03:26 +0530

    JAVASCRIPT

---
+++ Chapter 1/01_index.js
@@ -0,0 +1,5 @@
+console.log("Sahil khola ");
+// document.write("Sahil khola...");
+
+// What is JavaSCript?
+// JavaScript is a programming language that allows you to implement complex features on web pages.
